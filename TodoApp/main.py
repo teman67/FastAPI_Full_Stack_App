4005145f6cbd70e71d895eb988a6923e6cbd7670
Amirhossein Bayani <amirhossein.bayani@gmail.com>
2025-02-24 13:25:48 +0100

    add css file

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -3,12 +3,15 @@ from .models import Base
 from .database import engine
 from .routers import auth, todos, admin, users
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
 templates = Jinja2Templates(directory="TodoApp/templates")
+
+app.mount("/static", StaticFiles(directory="TodoApp/static"), name="static")
 
 @app.get("/")
 def test(request: Request):
